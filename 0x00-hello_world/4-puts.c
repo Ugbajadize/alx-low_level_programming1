144b4daa31e04be08d038bc79f4325a1de169d8e
Ugbajadize <talk2emmanuelugbaja@gmail.com>
2022-10-14 06:03:41 -0700

    Correct this

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
-#!/bin/bash
-#include <stdio.h>
+ #include <stdio.h>
+
 /**
  *
  *  * main - Entry point
@@ -7,5 +7,13 @@
  *   * Return: Always 0 (Success)
  *
  *    */
+
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle"); return (0);}
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
