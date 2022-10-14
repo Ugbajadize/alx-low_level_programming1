40cf039413a5ccd5e742c59adffdfc91e282e6d5
Ugbajadize <talk2emmanuelugbaja@gmail.com>
2022-10-14 08:30:53 -0700

    Continuing programme

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,12 @@
 #include <stdio.h>
-
-
-
 /**
+ * main - print a c program using printf
  *
- *  * main - Entry point
+ * Return:  0
  *
- *   * Return: Always 0 (Success)
- *
- *    */
-
+ */
 int main(void)
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
