145d4dcf4ac17209534059936c2369934472f0c1
Ugbajadize <talk2emmanuelugbaja@gmail.com>
2022-10-14 08:07:24 -0700

    First coding P

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/* 
+/*
  * main -Print a line of code using code
  *
  * Return: 0
 */
-int main(void) 
-{	
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
